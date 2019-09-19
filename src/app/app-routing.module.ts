a69b4423712acb89186a6c1c78397a8afb6f2531
PKRekha <rekha7.p@gmail.com>
2019-09-19 18:51:36 +0530

    Angular Basic example

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactusComponent } from './contactus/contactus.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'about',component:AboutComponent},
+  {path:'contactus',component:ContactusComponent},
+  {path:'',redirectTo:'about',pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { ContactusComponent } from './contactus/contactus.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ContactusComponent
   ],
   imports: [
     BrowserModule,
